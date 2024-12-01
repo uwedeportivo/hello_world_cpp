5cf383fb10977611b870c6ce8dce8cb7a557ab79
Uwe Hoffmann <534011+uwedeportivo@users.noreply.github.com>
2024-12-01 09:58:10 +0100

    use rocksdb

--- hello_world.cc
+++ hello_world.cc
@@ -12,6 +12,19 @@
 #include "marl/include/marl/scheduler.h"
 #include "marl/include/marl/waitgroup.h"
 #include "openssl/evp.h"
+#include "rocksdb/include/rocksdb/db.h"
+#include "rocksdb/include/rocksdb/options.h"
+#include "rocksdb/include/rocksdb/slice.h"
+#include "rocksdb/include/rocksdb/status.h"
+#include "rocksdb/include/rocksdb/write_batch.h"
+
+using ROCKSDB_NAMESPACE::DB;
+using ROCKSDB_NAMESPACE::Options;
+using ROCKSDB_NAMESPACE::PinnableSlice;
+using ROCKSDB_NAMESPACE::ReadOptions;
+using ROCKSDB_NAMESPACE::Status;
+using ROCKSDB_NAMESPACE::WriteBatch;
+using ROCKSDB_NAMESPACE::WriteOptions;
 
 
 template<typename T>
@@ -45,6 +58,8 @@ std::string computeSHA256Hex(const std::string& input) {
   return convertToHex(hash);
 }
 
+const std::string kDBPath = "/tmp/rocksdb_simple_example";
+
 int main() {
   const std::vector<std::string> v = {"foo", "bar", "baz"};
   const std::string s = absl::StrJoin(v, "-");
@@ -58,7 +73,7 @@ int main() {
   std::cout << "Joined string: " << s << "\n";
   std::cout << person.SerializeAsString() << "\n";
 
-  SQLite::Database db("/tmp/example.db3", SQLite::OPEN_READWRITE|SQLite::OPEN_CREATE);
+  SQLite::Database sqlitedb("/tmp/example.db3", SQLite::OPEN_READWRITE|SQLite::OPEN_CREATE);
 
   peg::parser parser(R"(
     # Grammar for Calculator...
@@ -113,5 +128,71 @@ int main() {
   auto res = cli.Get("/user?id=uwehn");
 
   std::cout << "body: " << res->body << "\n";
+
+  DB* db;
+  Options options;
+  // Optimize RocksDB. This is the easiest way to get RocksDB to perform well
+  options.IncreaseParallelism();
+  options.OptimizeLevelStyleCompaction();
+  // create the DB if it's not already present
+  options.create_if_missing = true;
+
+  // open DB
+  Status rocks_status = DB::Open(options, kDBPath, &db);
+  assert(rocks_status.ok());
+
+  // Put key-value
+  rocks_status = db->Put(WriteOptions(), "key1", "value");
+  assert(rocks_status.ok());
+  std::string value;
+  // get value
+  rocks_status = db->Get(ReadOptions(), "key1", &value);
+  assert(rocks_status.ok());
+  assert(value == "value");
+
+  // atomically apply a set of updates
+  {
+    WriteBatch batch;
+    batch.Delete("key1");
+    batch.Put("key2", value);
+    rocks_status = db->Write(WriteOptions(), &batch);
+  }
+
+  rocks_status = db->Get(ReadOptions(), "key1", &value);
+  assert(rocks_status.IsNotFound());
+
+  db->Get(ReadOptions(), "key2", &value);
+  assert(value == "value");
+
+  {
+    PinnableSlice pinnable_val;
+    db->Get(ReadOptions(), db->DefaultColumnFamily(), "key2", &pinnable_val);
+    assert(pinnable_val == "value");
+  }
+
+  {
+    std::string string_val;
+    // If it cannot pin the value, it copies the value to its internal buffer.
+    // The intenral buffer could be set during construction.
+    PinnableSlice pinnable_val(&string_val);
+    db->Get(ReadOptions(), db->DefaultColumnFamily(), "key2", &pinnable_val);
+    assert(pinnable_val == "value");
+    // If the value is not pinned, the internal buffer must have the value.
+    assert(pinnable_val.IsPinned() || string_val == "value");
+  }
+
+  PinnableSlice pinnable_val;
+  rocks_status = db->Get(ReadOptions(), db->DefaultColumnFamily(), "key1", &pinnable_val);
+  assert(rocks_status.IsNotFound());
+  // Reset PinnableSlice after each use and before each reuse
+  pinnable_val.Reset();
+  db->Get(ReadOptions(), db->DefaultColumnFamily(), "key2", &pinnable_val);
+  assert(pinnable_val == "value");
+  pinnable_val.Reset();
+  // The Slice pointed by pinnable_val is not valid after this point
+
+  delete db;
+
+
   return 0;
 }
