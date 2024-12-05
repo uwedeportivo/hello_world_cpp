1a9bb82ef9085c2a6e4e6adb31e44299915d8513
Uwe Hoffmann <534011+uwedeportivo@users.noreply.github.com>
2024-12-05 14:36:31 +0100

    llvm-project

--- hello_world.cc
+++ hello_world.cc
@@ -13,19 +13,7 @@
 #include "marl/include/marl/waitgroup.h"
 #include "openssl/evp.h"
 #include "rocksdb/include/rocksdb/db.h"
-#include "rocksdb/include/rocksdb/options.h"
-#include "rocksdb/include/rocksdb/slice.h"
-#include "rocksdb/include/rocksdb/status.h"
-#include "rocksdb/include/rocksdb/write_batch.h"
-
-using ROCKSDB_NAMESPACE::DB;
-using ROCKSDB_NAMESPACE::Options;
-using ROCKSDB_NAMESPACE::PinnableSlice;
-using ROCKSDB_NAMESPACE::ReadOptions;
-using ROCKSDB_NAMESPACE::Status;
-using ROCKSDB_NAMESPACE::WriteBatch;
-using ROCKSDB_NAMESPACE::WriteOptions;
-
+#include "llvm/Support/Debug.h"
 
 template<typename T>
 std::string convertToHex(const T& binaryResult)
@@ -129,8 +117,8 @@ int main() {
 
   std::cout << "body: " << res->body << "\n";
 
-  DB* db;
-  Options options;
+  rocksdb::DB* db;
+  rocksdb::Options options;
   // Optimize RocksDB. This is the easiest way to get RocksDB to perform well
   options.IncreaseParallelism();
   options.OptimizeLevelStyleCompaction();
@@ -138,35 +126,36 @@ int main() {
   options.create_if_missing = true;
 
   // open DB
-  Status rocks_status = DB::Open(options, kDBPath, &db);
+  rocksdb::Status rocks_status = rocksdb::DB::Open(options, kDBPath, &db);
   assert(rocks_status.ok());
 
   // Put key-value
-  rocks_status = db->Put(WriteOptions(), "key1", "value");
+  rocks_status = db->Put(rocksdb::WriteOptions(), "key1", "value");
   assert(rocks_status.ok());
   std::string value;
   // get value
-  rocks_status = db->Get(ReadOptions(), "key1", &value);
+  rocks_status = db->Get(rocksdb::ReadOptions(), "key1", &value);
   assert(rocks_status.ok());
   assert(value == "value");
 
   // atomically apply a set of updates
   {
-    WriteBatch batch;
+    rocksdb::WriteBatch batch;
     batch.Delete("key1");
     batch.Put("key2", value);
-    rocks_status = db->Write(WriteOptions(), &batch);
+    rocks_status = db->Write(rocksdb::WriteOptions(), &batch);
+    assert(rocks_status.ok());
   }
 
-  rocks_status = db->Get(ReadOptions(), "key1", &value);
+  rocks_status = db->Get(rocksdb::ReadOptions(), "key1", &value);
   assert(rocks_status.IsNotFound());
 
-  db->Get(ReadOptions(), "key2", &value);
+  db->Get(rocksdb::ReadOptions(), "key2", &value);
   assert(value == "value");
 
   {
-    PinnableSlice pinnable_val;
-    db->Get(ReadOptions(), db->DefaultColumnFamily(), "key2", &pinnable_val);
+    rocksdb::PinnableSlice pinnable_val;
+    db->Get(rocksdb::ReadOptions(), db->DefaultColumnFamily(), "key2", &pinnable_val);
     assert(pinnable_val == "value");
   }
 
@@ -174,19 +163,19 @@ int main() {
     std::string string_val;
     // If it cannot pin the value, it copies the value to its internal buffer.
     // The intenral buffer could be set during construction.
-    PinnableSlice pinnable_val(&string_val);
-    db->Get(ReadOptions(), db->DefaultColumnFamily(), "key2", &pinnable_val);
+    rocksdb::PinnableSlice pinnable_val(&string_val);
+    db->Get(rocksdb::ReadOptions(), db->DefaultColumnFamily(), "key2", &pinnable_val);
     assert(pinnable_val == "value");
     // If the value is not pinned, the internal buffer must have the value.
     assert(pinnable_val.IsPinned() || string_val == "value");
   }
 
-  PinnableSlice pinnable_val;
-  rocks_status = db->Get(ReadOptions(), db->DefaultColumnFamily(), "key1", &pinnable_val);
+  rocksdb::PinnableSlice pinnable_val;
+  rocks_status = db->Get(rocksdb::ReadOptions(), db->DefaultColumnFamily(), "key1", &pinnable_val);
   assert(rocks_status.IsNotFound());
   // Reset PinnableSlice after each use and before each reuse
   pinnable_val.Reset();
-  db->Get(ReadOptions(), db->DefaultColumnFamily(), "key2", &pinnable_val);
+  db->Get(rocksdb::ReadOptions(), db->DefaultColumnFamily(), "key2", &pinnable_val);
   assert(pinnable_val == "value");
   pinnable_val.Reset();
   // The Slice pointed by pinnable_val is not valid after this point
