4a2fb8b7d4fd88016726e2682c33b3409dac2528
Uwe Hoffmann <534011+uwedeportivo@users.noreply.github.com>
2025-03-23 14:34:25 -0700

    bazel modules upgrade

--- hello_world.cc
+++ hello_world.cc
@@ -6,14 +6,9 @@
 #include "greeting.pb.h"
 #include "SQLiteCpp/include/SQLiteCpp/Database.h"
 #include "cpp-peglib/peglib.h"
-#include "cpp-httplib/httplib.h"
-#include "marl/include/marl/defer.h"
-#include "marl/include/marl/event.h"
-#include "marl/include/marl/scheduler.h"
-#include "marl/include/marl/waitgroup.h"
+#include "httplib.h"
 #include "openssl/evp.h"
-#include "rocksdb/include/rocksdb/db.h"
-#include "llvm/Support/Debug.h"
+#include "include/rocksdb/db.h"
 
 template<typename T>
 std::string convertToHex(const T& binaryResult)
@@ -71,41 +66,6 @@ int main() {
     Number      <- < [0-9]+ >
     %whitespace <- [ \t]*
   )");
-
-  marl::Scheduler scheduler(marl::Scheduler::Config::allCores());
-  scheduler.bind();
-  defer(scheduler.unbind());  // Automatically unbind before returning.
-
-  constexpr int numTasks = 10;
-
-  // Create an event that is manually reset.
-  marl::Event sayHello(marl::Event::Mode::Manual);
-
-  // Create a WaitGroup with an initial count of numTasks.
-  marl::WaitGroup saidHello(numTasks);
-
-  // Schedule some tasks to run asynchronously.
-  for (int i = 0; i < numTasks; i++) {
-    // Each task will run on one of the 4 worker threads.
-    marl::schedule([=] {  // All marl primitives are capture-by-value.
-      // Decrement the WaitGroup counter when the task has finished.
-      defer(saidHello.done());
-
-      printf("Task %d waiting to say hello...\n", i);
-
-      // Blocking in a task?
-      // The scheduler will find something else for this thread to do.
-      sayHello.wait();
-
-      printf("Hello from task %d!\n", i);
-    });
-  }
-
-  sayHello.signal();  // Unblock all the tasks.
-
-  saidHello.wait();  // Wait for all tasks to complete.
-
-  printf("All tasks said hello.\n");
 
   auto ss = computeSHA256("Hello world!");
 
